d92e1449d4dabc783bfe9ed458ae5cdab3d4b871
kurves <kavssimply@gmail.com>
2024-02-27 10:30:14 -0800

    rege to match repeating string

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def match_string(str)
+  regex = /hbt{2,5}n/
+  if regex.match(str)
+    puts "String '#{str}' matches the pattern"
+  else
+    puts "String '#{str}' does not match the pattern"
+  end
+end
+
+if ARGV.length != 1
+  puts "Usage: ruby script.rb <string>"
+  exit 1
+end
+
+string = ARGV[0]
+match_string(string)
