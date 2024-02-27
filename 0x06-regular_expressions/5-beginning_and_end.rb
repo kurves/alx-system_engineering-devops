2b41b4f90ea4e7a9ceb7c09d66c86cb9ac5fa429
kurves <kavssimply@gmail.com>
2024-02-27 10:37:03 -0800

    rregex to match beginning and end

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def match_string(str)
+  regex = /^h.n$/
+  if regex.match(str)
+    puts "$"
+  else
+    puts "String '#{str}' does not match the pattern"
+  end
+end
+
+if ARGV.length != 1
+  puts "Usage: ruby script.rb <string>"
+  exit 1
+end
+
+string = ARGV[0]
+match_string(string)
